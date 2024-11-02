cb00a2e7a34b46b5a891e56608aa238d86e21b62
CesarZalamea <cesar.zalameaj@ug.edu.ec>
2024-11-01 21:31:18 -0500

    se termina nuevo activity falta probarlo y arreglar cualquier bug, ademas de agregar fondo de pantalla para el nuevo activitu del registro

--- app/src/main/java/com/raccoon/gestorc3/RegistroActivity.java
+++ app/src/main/java/com/raccoon/gestorc3/RegistroActivity.java
@@ -1,7 +1,14 @@
 package com.raccoon.gestorc3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +17,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private EditText cedula, nombres, apellidos, edad;
+    private RatingBar ratingNivelIngles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +31,68 @@ public class RegistroActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //conf spíners
+        Spinner spinnerNacionalidad = findViewById(R.id.spinnerNacionalidad);
+        Spinner spinnerGenero = findViewById(R.id.spinnerGenero);
+
+
+        ArrayAdapter<CharSequence> nacionalidadAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.nacionalidad_array,
+                android.R.layout.simple_spinner_item);
+        nacionalidadAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerNacionalidad.setAdapter(nacionalidadAdapter);
+
+        ArrayAdapter <CharSequence> generoAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.genero_array,
+                android.R.layout.simple_spinner_item
+        );
+
+        generoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGenero.setAdapter(generoAdapter);
+
+        //configurando los botones
+
+        Button btnRegistrar = findViewById(R.id.btnRegistrar);
+        Button btnCancelar = findViewById(R.id.button2);
+        Button btnBorrar = findViewById(R.id.button3);
+
+        //boton registrar
+        btnRegistrar.setOnClickListener(v -> {
+            //obtener los datos de los campos de texto
+            String datos = "Cedula" +cedula.getText().toString() +
+                    "\nNombres" +nombres.getText().toString() +
+                    "\nApellidos" +apellidos.getText().toString() +
+                    "\nEdad" +edad.getText().toString() +
+                    "\nNacionalidad" +spinnerNacionalidad.getSelectedItem().toString() +
+                    "\nGenero" +spinnerGenero.getSelectedItem().toString() +
+                    "\nNivel de Ingles" +ratingNivelIngles.getRating();
+
+            //MOSTRAMOS LOS DATOS EN EL LOGCAT CONSOLA
+            Log.d("Datos", datos);
+            Toast.makeText(this, "Datos registrados", Toast.LENGTH_SHORT).show();
+        });
+
+        //boton borrar
+        btnBorrar.setOnClickListener(view -> {
+            //limpiar los campos de texto
+            cedula.setText("");
+            nombres.setText("");
+            apellidos.setText("");
+            edad.setText("");
+            spinnerNacionalidad.setSelection(0);
+            spinnerGenero.setSelection(0);
+            ratingNivelIngles.setRating(0);
+            Toast.makeText(this, "Campos borrados", Toast.LENGTH_SHORT).show();
+
+        });
+
+        //boton cancelar
+        btnCancelar.setOnClickListener(view -> {
+            //regresamos al login
+            Intent intent = new Intent(this, SplashActivity.class);
+        });
     }
-
-    Spinner spinnerNacionalidad = findViewById(R.id.spinner);
-    Spinner spinnerGenero = findViewById(R.id.spinner);
-
 }
